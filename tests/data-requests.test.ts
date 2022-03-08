dfc46b42705531a6504fde7c5c2da32e5de40798
Daniil Bratchenko <bratchenko@gmail.com>
2022-03-08 12:43:32 +0200

    cleaned up legacy connector

--- tests/data-requests.test.ts
+++ tests/data-requests.test.ts
@@ -1,18 +1,9 @@
 import { makeRequest } from './config'
 
 describe('Data Requests', () => {
-  it('should return dataRequest in the root request response', async () => {
-    const rootResponse = await makeRequest('')
+  it('should return persons collection', async () => {
+    const rootDataResponse = await makeRequest('/data/collections/persons')
 
-    expect(rootResponse.body).toHaveProperty('dataRequest')
-
-    const rootDataResponse = await makeRequest(
-      rootResponse.body.dataRequest.key,
-      rootResponse.body.dataRequest.payload,
-    )
-
-    expect(rootDataResponse.body).toHaveProperty('links')
-    const links = rootDataResponse.body.links
-    expect(links.items).toHaveLength(9)
+    expect(rootDataResponse.body).toHaveProperty('insert')
   })
 })
