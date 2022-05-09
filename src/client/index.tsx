88d58c388973a042e1a12956916bee6304886350
Daniil Bratchenko <bratchenko@gmail.com>
2022-05-09 13:41:01 -0400

    typo

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
 import { FormComposer, FormInputType } from '@integration-app/ui'
-import NewConnectionPage from '@integratio-app/ui/NewConnectionPage'
+import NewConnectionPage from '@integration-app/ui/NewConnectionPage'
 
 const schema = [
   {
