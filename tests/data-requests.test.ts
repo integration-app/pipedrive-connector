4e8bcb2ac6a6446f20a18e8ae39200cf8475f0d3
maxim-bobritsky <20352355+maxim-bobritsky@users.noreply.github.com>
2022-06-07 15:46:44 +0300

    rm old tests

--- tests/data-requests.test.ts
+++
@@ -1,9 +0,0 @@
-import { makeRequest } from './config'
-
-describe('Data Requests', () => {
-  it('should return persons collection', async () => {
-    const response = await makeRequest('/data/persons')
-
-    expect(response.body).toHaveProperty('fieldsSchema')
-  })
-})
