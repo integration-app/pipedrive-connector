5264fe1356f5f4f49d200b9114f6a73af26552ff
maxim-bobritsky <20352355+maxim-bobritsky@users.noreply.github.com>
2022-06-10 15:41:58 +0300

    add Leads schema

---
+++ src/collections/leads/index.ts
@@ -0,0 +1,28 @@
+import { Type } from '@sinclair/typebox'
+import { objectCollectionHandler } from '../common'
+
+const MODIFIABLE_FIELDS = [
+  'title',
+  'owner_id',
+  'label_ids',
+  'person_id',
+  'organization_id',
+  'expected_close_date',
+]
+
+const leads = objectCollectionHandler({
+  path: 'leads',
+  name: 'Leads',
+  createFields: MODIFIABLE_FIELDS,
+  updateFields: MODIFIABLE_FIELDS,
+  requiredFields: ['title'], // actually, you also has to provide either organization_id or person_id
+  lookupFields: ['title'],
+  udm: 'leads',
+})
+
+export default leads
+
+export const LEAD_SCHEMA = Type.Integer({
+  title: 'Lead',
+  referenceCollectionUri: leads.uri,
+})
