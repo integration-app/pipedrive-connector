77e01533ec699511ae477df4694daff1ee4557ec
maxim-bobritsky <20352355+maxim-bobritsky@users.noreply.github.com>
2022-06-23 16:20:41 +0300

    read the test features from yaml

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,141 +1,22 @@
-import { UNIFIED_DATA_MODELS } from '@integration-app/sdk/udm/index'
-import * as random from 'generate-random-data'
-import { makeRequest } from './config'
-
-const SUPPORTED_FEATURES = {
-  test: true,
-  udm: {
-    contacts: {
-      unifiedFields: ['fullName', 'email', 'companyId', 'ownerId'],
-      collectionName: 'persons',
-      updatableFields: ['name'], // make it work for email and phone that are Arrays
-      create: true,
-      find: true,
-      update: true,
-      events: false,
-    },
-    // leads: {
-    //   unifiedFields: ['name', 'companyId', 'ownerId'],
-    //   collectionName: 'leads',
-    //   updatableFields: ['title', 'expected_close_date'],
-    //   create: true,
-    //   find: true,
-    //   update: false,
-    //   events: false,
-    // },
-    companies: {
-      unifiedFields: ['name', 'ownerId'],
-      collectionName: 'organizations',
-      updatableFields: ['name'],
-      find: true,
-      create: true,
-      update: true,
-      events: false,
-    },
-    activities: {
-      unifiedFields: [
-        'description',
-        'title',
-        'dealId',
-        'contactId',
-        'companyId',
-        'ownerId',
-      ],
-      collectionName: 'activities',
-      updatableFields: ['subject'],
-      find: true,
-      create: true,
-      update: true,
-      events: false,
-    },
-    deals: {
-      unifiedFields: ['name', 'ownerId', 'companyId', 'amount'],
-      collectionName: 'deals',
-      updatableFields: ['title', 'value'],
-      find: true,
-      create: true,
-      update: true,
-      events: false,
-    },
-    // users: {
-    //   unifiedFields: ['fullName', 'email', 'isActive'],
-    //   collectionName: 'users',
-    //   updatableFields: ['name', 'email'],
-    //   find: true,
-    //   create: true,
-    //   update: true,
-    //   events: false,
-    // },
-  },
-}
-// generate random data based on types (from UNIFIED_DATA_MODELS in sdk)
-function fillWithValue(field: string) {
-  switch (field) {
-    case 'fullName':
-      return random.name('Von')
-    case 'email':
-      return random.email('test.org')
-    case 'phone':
-      return random.mobile().toString()
-    case 'amount':
-      return random.integer(0, 1000000)
-    case 'value':
-      return random.integer(0, 10000)
-    case 'expected_close_date':
-      return random.randomDate()
-    case 'isActive':
-      return false
-    case 'name':
-      return 'Test Name - ' + random.integer()
-    case 'companyName':
-      return 'Test Company - ' + random.id()
-    default:
-      return 'Test Default - ' + random.id()
-  }
-}
-
-function generateRandomValues(unifiedFields: string[]) {
-  return unifiedFields.reduce((fields, field) => {
-    fields[field] = fillWithValue(field)
-    return fields
-  }, {})
-}
-
-const dereference = async (
-  udm: string,
-  collection: string,
-  fieldsToFind: string[],
-) => {
-  // getting the refernce values from the first collection record
-  const res = await makeRequest(`/data/${collection}/find`, {
-    udm: udm,
-  })
-  const data = res.records
-  const references = {}
-  // for every record in data, check if the field is in the fieldsToFind array
-  // if it is, get the value of the field and add it to the references object
-  // stop when all the fieldsToFind are found
-  for (const record of data) {
-    for (const field of fieldsToFind) {
-      if (record.unifiedFields[field]) {
-        references[field] = record.unifiedFields[field]
-      }
-    }
-    if (Object.keys(references).length === fieldsToFind.length) {
-      break
-    }
-  }
-  return references
-}
+import * as fs from 'fs'
+import * as yaml from 'js-yaml'
+import {
+  dereference,
+  extractReferences,
+  generateRandomValues,
+  makeRequest,
+} from './config'
 
 describe('UDM', () => {
   let spec: any
+  const data = yaml.load(fs.readFileSync('./tests/supported-features.yaml'))
 
   beforeEach(async () => {
     spec = await makeRequest('/')
   })
-  for (const collection in SUPPORTED_FEATURES.udm) {
-    const collectionProperties = SUPPORTED_FEATURES.udm[collection]
+  for (const collection in data) {
+    const collectionProperties = data[collection]
+    const collectionActions = collectionProperties.actions
     const basicFieldValues = generateRandomValues(
       collectionProperties.unifiedFields,
     )
@@ -148,7 +29,7 @@ describe('UDM', () => {
       it(`should perform operations on ${collection}`, async () => {
         const references = await dereference(
           collection,
-          collectionProperties.collectionName,
+          collectionProperties.key,
           fieldsWithReference,
         )
         const collectionUri = spec.data[collection].uri
@@ -166,7 +47,7 @@ describe('UDM', () => {
         console.log(`fields: ${JSON.stringify(fields)}`)
 
         let newRecordId = null
-        if (collectionProperties.create) {
+        if (collectionActions.includes('create')) {
           const createResponse = await makeRequest(`${collectionUri}/create`, {
             fields,
           })
@@ -174,7 +55,7 @@ describe('UDM', () => {
           newRecordId = createResponse.id
           console.log(`Created ${collection} with id: ${newRecordId}`)
         }
-        if (collectionProperties.find) {
+        if (collectionActions.includes('find')) {
           const findByIdResponse = await makeRequest(
             `${collectionUri}/find-by-id`,
             {
@@ -186,7 +67,7 @@ describe('UDM', () => {
             unifiedFields,
           )
         }
-        if (collectionProperties.update) {
+        if (collectionActions.includes('update')) {
           const fieldsToUpdate = generateRandomValues(
             collectionProperties.updatableFields,
           )
@@ -215,17 +96,3 @@ describe('UDM', () => {
     })
   }
 })
-function extractReferences(collection: string): string[] {
-  const udmFieldsDescription =
-    UNIFIED_DATA_MODELS[collection].fieldsSchema.properties
-  const fieldsWithReference = Object.keys(udmFieldsDescription).filter(
-    (field) => {
-      return (
-        udmFieldsDescription[field].referenceUdm &&
-        field !== 'createdBy' &&
-        field !== 'updatedBy'
-      )
-    },
-  )
-  return fieldsWithReference
-}
