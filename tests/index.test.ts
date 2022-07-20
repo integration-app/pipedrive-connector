4dbd3e1233ab98d9cc32b0a0c1977059c06c997e
Daniil Bratchenko <bratchenko@gmail.com>
2022-07-20 18:53:28 -0400

    Use SDK-provided tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,78 +1,9 @@
-import * as fs from 'fs'
-import * as yaml from 'js-yaml'
-import { generateFieldUpdates, makeRequest } from './config'
+import {
+  unifiedFieldsTest,
+  basicFunctionalityTest,
+} from '@integration-app/connector-sdk'
 
-describe('UDM', () => {
-  let spec: any
-  const collections = yaml.load(fs.readFileSync('supported-features.yaml'))
-
-  beforeEach(async () => {
-    spec = await makeRequest('/')
-  })
-  for (const collection in collections) {
-    const collectionProperties = collections[collection]
-    const collectionActions = collectionProperties.actions
-    const udmFields = collectionProperties.unifiedFields
-    let newRecordId = null
-
-    describe(`${collection}`, () => {
-      it(`should have ${collection} UDM`, async () => {
-        expect(spec.data[collection]).toBeDefined()
-      })
-      if (collectionActions.includes('create')) {
-        it(`should create ${collection}`, async () => {
-          const collectionUri = spec.data[collection].path
-          const fieldUpdates = await generateFieldUpdates(
-            collection,
-            collectionUri,
-            udmFields,
-          )
-          const createResponse = await makeRequest(`${collectionUri}/create`, {
-            fields: fieldUpdates.fields,
-          })
-          console.log(createResponse)
-          expect(createResponse.id).toBeDefined()
-          newRecordId = createResponse.id
-          console.log(`Created ${collection} with id: ${newRecordId}`)
-        })
-      }
-      if (collectionActions.includes('find-by-id')) {
-        it(`should find created ${collection} by id`, async () => {
-          const collectionUri = spec.data[collection].path
-          const findByIdResponse = await makeRequest(
-            `${collectionUri}/find-by-id`,
-            {
-              id: newRecordId,
-              udm: collection,
-            },
-          )
-          expect(findByIdResponse.record.id.toString()).toBe(newRecordId)
-        })
-      }
-      if (collectionActions.includes('update')) {
-        it(`should update created ${collection}`, async () => {
-          const collectionUri = spec.data[collection].path
-          const fieldsToUpdate = await generateFieldUpdates(
-            collection,
-            collectionUri,
-            collectionProperties.updatableFields,
-          )
-          const updatedRecord = await makeRequest(`${collectionUri}/update`, {
-            id: newRecordId,
-            fields: fieldsToUpdate.fields,
-          })
-          const findUpdatedRecord = await makeRequest(
-            `${collectionUri}/find-by-id`,
-            {
-              id: updatedRecord.id,
-              udm: collection,
-            },
-          )
-          expect(findUpdatedRecord.record.unifiedFields).toMatchObject(
-            fieldsToUpdate.unifiedFields,
-          )
-        })
-      }
-    })
-  }
+describe('Connector Test', () => {
+  basicFunctionalityTest()
+  unifiedFieldsTest()
 })
