ed894d1af065038993e5c5965dbc4d992e2b858f
ReyBroncas <andriksmen@gmail.com>
2022-11-18 18:05:22 +0100

    bump sdk

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -7,30 +7,36 @@ import { TestRunner } from '@integration-app/connector-sdk'
 const runner = new TestRunner({
   baseUri: process.env.BASE_URI,
   accessToken: process.env.TEST_ACCESS_TOKEN,
-  skipFields: {
-    '/data/deals': [
-      'probability', // If we try to set probability in some pipelines, it will raise an error 'Deal probability is not enabled on this pipeline.'
-      'lost_reason', // custom lost_reason is not returned in events for some reason - may be a bug on our side.
-    ],
-    '/data/persons': ['marketing_status'],
-    '/data/activities': ['due_time', 'duration'], // Due time and duration are being converted to HH:MM (from HH:MM:ss) and it gets mismatched
-  },
   queryDelay: 5000,
   filter: process.argv[2],
-  fieldValueGenerators: {
-    '/data/products': {
-      prices: () => [
-        {
-          id: 612646,
-          product_id: 807027,
-          price: 243329,
-          currency: 'USD',
-          cost: 926549,
-          overhead_cost: 154251,
-        },
+  collections: {
+    '/data/deals': {
+      skipFields: [
+        'probability', // If we try to set probability in some pipelines, it will raise an error 'Deal probability is not enabled on this pipeline.'
+        'lost_reason', // custom lost_reason is not returned in events for some reason - may be a bug on our side.
       ],
-      visible_to: () => 3,
-      code: () => '1',
+    },
+    '/data/persons': {
+      skipFields: ['marketing_status'],
+    },
+    '/data/activities': {
+      skipFields: ['due_time', 'duration'], // Due time and duration are being converted to HH:MM (from HH:MM:ss) and it gets mismatched
+    },
+    '/data/products': {
+      genFieldValues: {
+        prices: () => [
+          {
+            id: 612646,
+            product_id: 807027,
+            price: 243329,
+            currency: 'USD',
+            cost: 926549,
+            overhead_cost: 154251,
+          },
+        ],
+        visible_to: () => 3,
+        code: () => '1',
+      },
     },
   },
 })
