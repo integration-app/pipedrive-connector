44a902f09200a2c1a6fd0f572efbd7d67dbd81cf
Daniil Bratchenko <bratchenko@gmail.com>
2023-01-18 19:22:20 +0100

    bettter client

--- src/auth/make-api-client.ts
+++ src/auth/make-api-client.ts
@@ -1,19 +1,13 @@
 import { RestApiClient } from '@integration-app/connector-sdk'
 import {
+  BadRequestError,
   ConnectionError,
   ConnectionErrorKey,
 } from '@integration-app/sdk/errors'
 
 export default function makeApiClient({ credentials }) {
-  return new RestApiClient({
+  const params: any = {
     baseUri: 'https://api.pipedrive.com/v1',
-    headers: {
-      Authorization: `Bearer ${credentials.access_token}`,
-    },
-    // Backward compatibility for connections that use api_token.
-    query: {
-      api_token: credentials.api_token,
-    },
     onError: (error) => {
       if (error.response.status === 401) {
         throw new ConnectionError({
@@ -22,5 +16,22 @@ export default function makeApiClient({ credentials }) {
         })
       }
     },
-  })
+  }
+
+  if (credentials.access_token) {
+    params.headers = {
+      Authorization: `Bearer ${credentials.access_token}`,
+    }
+  } else if (credentials.api_token) {
+    // Backward compatibility for connections that use api_token.
+    params.query = {
+      api_token: credentials.api_token,
+    }
+  } else {
+    throw new BadRequestError(
+      'Either access_token or api_token must be provided',
+    )
+  }
+
+  return new RestApiClient(params)
 }
